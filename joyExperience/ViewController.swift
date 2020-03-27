674e9f7bc7fa34072c80d72cbe0715029c9f1974
michael tamsil <michaeltamsil@gmail.com>
2020-03-28 06:46:01 +0700

    Signed-off-by: michael tamsil <michaeltamsil@gmail.com>
    new initial app

--- joyExperience/ViewController.swift
+++ joyExperience/ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
+    
+    @IBAction func btnMulai(_ sender: Any) {
 
-
+    }
+    
 }
 
